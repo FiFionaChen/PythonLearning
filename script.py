b5b578825f69bb3b344c92ba2fba37167f925e5b
FiFionaChen <yifeichen90@gmail.com>
2019-04-11 11:48:17 +1200

    Added test objects

---
+++ script.py
@@ -0,0 +1,3 @@
+destinations = ["Paris, France", "Shanghai, China", "LosAngeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
